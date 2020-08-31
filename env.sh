901da94fca62585479564034ca51c51fedfe8425
jasonjx-w <jasonjx.w@outlook.com>
2020-08-31 20:43:31 +0800

    [Make]: update git commit-template.

--- env.sh
+++ env.sh
@@ -5,3 +5,13 @@ export PATH=$PATH:/${PWD}/build/bin
 export LIBRARY_PATH=$LIBRARY_PATH:/${PWD}/build/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/${PWD}/build/lib
 
+if [ -d "${PWD}/.git/" ]; then
+  if [ -f "${PWD}/tools/commit-template" ]; then
+    git config --local commit.template tools/commit-template
+  fi
+
+  if [ -f "${PWD}/.git/hooks/pre-commit" ]; then
+    rm -r ${PWD}/.git/hooks/pre-commit
+  fi
+  ln -sf ${PWD}/tools/pre-commit ${PWD}/.git/hooks
+fi
